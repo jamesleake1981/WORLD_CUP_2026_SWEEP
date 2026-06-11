a9c18a24edbb12a0fd0d71cd76908be99388e0ae
jamesleake1981 <jleake1981@gmail.com>
2026-06-11 18:21:43 -0400

    Update matches.py

--- matches.py
+++ matches.py
@@ -1,5 +1,4 @@
 matches = [
-# home, home_goals, away, away_goals, home_yc, home_rc, away_yc, away_rc
-    #("Mexico", 2, "South Africa", 1, 1, 1, 1, 2),
-    #("South Africa", 3, "South Korea", 1, 2, 3, 1, 1)
+ home, home_goals, away, away_goals, home_yc, home_rc, away_yc, away_rc
+    ("Mexico", 2, "South Africa", 0, 1, 1, 2, 2)
         ]
